3aa0ad8d11536945547ec9e165e6cefa85e4409e
jwzimmer <jzimmer1@uvm.edu>
2021-01-14 15:22:00 -0500

    works that are in the 2nd wget result for Literature but not the first
    maybe that's fine? i'm not sure how to make sure we really have all of them scraped...

--- delete_extra_files_literature.py
+++ delete_extra_files_literature.py
@@ -8,6 +8,19 @@ Created on Wed Jan 13 15:56:03 2021
 
 import os
 import re
+import json
+
+# === === Helper functions === ===
+def write_json(data, filename):
+    with open(filename,"w") as f:
+        json.dump(data,f)
+    return None
+def get_json(filename):
+    with open(filename) as f:
+        jsonobj = json.load(f)
+    return jsonobj
+    
+# === === === === === === === === 
 
 def delete_extras(filename):
     pathname = filename
@@ -26,8 +39,23 @@ def delete_extras(filename):
     
 def foldercont(foldername):
     allfiles = os.listdir(foldername)
-    for f in allfiles:
-        delete_extras(foldername+"/"+f)
-    return None
-    
-foldercont("tvtropes.org-attempt/pmwiki/pmwiki.php/Literature")
+    return allfiles
+
+def compare_folders():
+    attemptfiles = foldercont("tvtropes.org-attempt/pmwiki/pmwiki.php/Literature")
+    files = foldercont("tvtropes.org/pmwiki/pmwiki.php/Literature")
+    different_files = []
+    for f in attemptfiles:
+        if f not in files:
+            different_files.append(f)
+            print("In attempt only: ",f)
+    for f in files:
+        if f not in attemptfiles:
+            different_files.append(f)
+            print("In plain only: ",f)
+    write_json(different_files, "difference_in_Lit_wget_results.json")
+    return different_files
+
+compare_folders()
+
+
