4d60919354329eebfb07a974716c4c77b73cd737
jwzimmer <jzimmer1@uvm.edu>
2021-01-15 10:51:09 -0500

    combining all the files from the 3 literature attempts (union) into Literature-Union
    taking most recent version of file available from the 3 result sets

--- delete_extra_files_literature.py
+++ delete_extra_files_literature.py
@@ -45,27 +45,43 @@ def foldercont(foldername):
 
 def compare_folders():
     attemptfiles = foldercont("tvtropes.org-attempt/pmwiki/pmwiki.php/Literature")
+    attemptname = "tvtropes.org-attempt/pmwiki/pmwiki.php/Literature/"
     files = foldercont("tvtropes.org-plain/pmwiki/pmwiki.php/Literature")
+    filesname = "tvtropes.org-plain/pmwiki/pmwiki.php/Literature/"
     thirdfiles = foldercont("tvtropes.org-third/pmwiki/pmwiki.php/Literature")
-    for f in thirdfiles:
-        delete_extras("tvtropes.org-third/pmwiki/pmwiki.php/Literature/" + f)
-    thirdfiles = foldercont("tvtropes.org-third/pmwiki/pmwiki.php/Literature")
-    different_files = []
-    filelist = []
-    for f in attemptfiles:
-        if f not in files and f not in thirdfiles:
-            different_files.append(f)
-            filelist += ["In attempt only: tvtropes.org-attempt/pmwiki/pmwiki.php/Literature",f]
-    for f in files:
-        if f not in attemptfiles and f not in thirdfiles:
-            different_files.append(f)
-            filelist += ["In plain only: tvtropes.org-plain/pmwiki/pmwiki.php/Literature",f]
-    for f in thirdfiles:
-        if f not in files and f not in attemptfiles:
-            different_files.append(f)
-            filelist += ["In third only: tvtropes.org-third/pmwiki/pmwiki.php/Literature",f]
-    write_json(filelist, "difference_in_paths_3_Lit_wget_results_and.json")
-    return different_files
+    thirdname = "tvtropes.org-third/pmwiki/pmwiki.php/Literature/"
+    # for f in thirdfiles:
+    #     delete_extras("tvtropes.org-third/pmwiki/pmwiki.php/Literature/" + f)
+    # thirdfiles = foldercont("tvtropes.org-third/pmwiki/pmwiki.php/Literature")
+    # different_files = []
+    # filelist = []
+    # for f in attemptfiles:
+    #     if f not in files and f not in thirdfiles:
+    #         different_files.append(f)
+    #         filelist += ["In attempt only: tvtropes.org-attempt/pmwiki/pmwiki.php/Literature",f]
+    # for f in files:
+    #     if f not in attemptfiles and f not in thirdfiles:
+    #         different_files.append(f)
+    #         filelist += ["In plain only: tvtropes.org-plain/pmwiki/pmwiki.php/Literature",f]
+    # for f in thirdfiles:
+    #     if f not in files and f not in attemptfiles:
+    #         different_files.append(f)
+    #         filelist += ["In third only: tvtropes.org-third/pmwiki/pmwiki.php/Literature",f]
+    # write_json(filelist, "difference_in_paths_3_Lit_wget_results_and.json")
+    os.mkdir("Literature-Union")
+    unionname = "Literature-Union/"
+    unionfiles = list(set(attemptfiles + files + thirdfiles))
+    write_json(unionfiles, "union_of_3_Lit_wget_results.json")
+    for f in unionfiles:
+        if f in thirdfiles:
+            os.rename(thirdname+f, unionname+f)
+        elif f in files:
+            os.rename(filesname+f, unionname+f)
+        elif f in attemptfiles:
+            os.rename(attemptname+f, unionname+f)
+        else:
+            print("where did this come from: ",f)    
+    return None
 
 compare_folders()
 
