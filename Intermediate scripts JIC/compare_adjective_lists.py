c24f941be97581af1ea54b46ef1816b81428f816
jwzimmer <jzimmer1@uvm.edu>
2021-10-18 17:59:51 -0400

    add script for comparing lists of adjectives in venn diagram and latex or markdown table

---
+++ Intermediate scripts JIC/compare_adjective_lists.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  6 15:47:38 2021
+
+@author: jzimmer1
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 28 13:55:13 2021
+
+@author: jzimmer1
+"""
+
+import pandas as pd
+from pandas_ods_reader import read_ods
+import json
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import scipy as sp
+from numpy import linalg
+from scipy import linalg
+from collections import Counter
+from mpl_toolkits.mplot3d import Axes3D
+import random
+from matplotlib_venn import venn2, venn2_circles
+import pprint
+
+# === === Helper functions === ===
+def write_json(data, filename):
+    with open(filename,"w") as f:
+        json.dump(data,f)
+    return None
+
+def get_json(filename):
+    with open(filename) as f:
+        jsonobj = json.load(f)
+    return jsonobj
+
+# === === Other artifacts that might be useful === ===
+# # Load the original data as dataframes
+# path="characters-aggregated.ods"
+# df_original = pd.read_json("June2021_df_original.json")
+# df_n_original =  pd.read_json("June2021_df_n_original.json")
+# df_std_original =  pd.read_json("June2021_df_std_original.json")
+# column_dict_original = get_json("June2021_column_dict_original.json")
+
+# # Load the tables that tell you what the BAP traits are and who the characters are
+# character_map, bap_map = pd.read_html("codebook.html")
+
+
+df_bap = pd.read_json("July2021_df_bap.json")
+df_traits = pd.read_json("July2021_df_traits.json")
+
+clean_column_dict = get_json("July2021_cleaned_column_dict.json")
+
+def runSVD(df1,dropcols=['unnamed.1','name','work'],n=None):
+    if len(dropcols) > 0:
+        for x in dropcols:
+            df1 = df1.drop(x,axis=1)
+    if n==None:
+        n=df1.shape[1]-1
+    X = df1.to_numpy()
+    #decompose
+    U, D, V = np.linalg.svd(X)
+    # get dim of X
+    M,N = X.shape
+    # Construct sigma matrix in SVD (it simply adds null row vectors to match the dim of X)
+    Sig = sp.linalg.diagsvd(D,M,N)
+    # Now you can get X back:
+    remakeX = np.dot(U, np.dot(Sig, V))
+    assert np.sum(remakeX - X) < 0.00001
+    return df1, U, D, V, Sig, X, remakeX
+
+# Output from SVD without removing means
+df1, U, D, V, Sig, X, remakeX = runSVD(df_traits)
+
+# Remove the average of each trait
+#df1_means = df1.mean().mean()
+df1_means = 50
+df1_normed = df1 - df1_means
+
+# Output from SVD WITH removing means
+df2, U2, D2, V2, Sig2, X2, remakeX2 = runSVD(df1_normed,dropcols=[])
+# When remaking X, Sig2 and V2 are combined, then their product is combined with U2, so here is that first product
+SigV2 = np.dot(Sig2,V2)
+# the traits in order of columns
+col2 = df2.columns
+
+def get_adj(col_list=list(col2)):
+    adj_list = []
+    for pair in col_list:
+        left, right = pair.split("<->")
+        adj_list += [left, right]
+    return set(adj_list)
+
+# just the adjectives
+adj_list = get_adj()
+
+# https://projects.ori.org/lrg/PDFs_papers/Goldberg.Big-Five-Markers-Psych.Assess.1992.pdf
+table_2_bipolar = ["silent","talkative","timid","bold","compliant","assertive","inhibited","spontaneous","passive","active","reserved","demonstrative","lethargic","energetic","apathetic","enthusiastic","unadventurous","adventurous","unsociable","sociable",
+                   "cold","warm","unkind","kind","uncooperative","cooperative","selfish","unselfish","rude","polite","distrustful","trustful","stingy","generous","stubborn","flexible","inconsiderate","considerate","quarrelsome","agreeable",
+                   "disorganized","organized","undependable","dependable","unconscientious","conscientious","impractical","practical","careless","thorough","extravagant","thrifty","rash","cautious","frivolous","serious","wasteful","economical","unreliable","reliable",
+                   "emotional","unemotional","envious","unenvious","nervous","relaxed","subjective","objective","high-strung","imperturbable","demanding","undemanding","fretful","placid","volatile","peaceful","suggestible","independent","fearful","uninhibited",
+                   "unintelligent","intelligent","imperceptive","perceptive","uninquisitve","curious","unimaginative","imaginative","uncreative","creative","unsophisticated","sophisticated","ignorant","knowledgeable","unintellectual","intellectual","shallow","deep","provincial","cultured"]
+
+table_2_bi = set(table_2_bipolar)
+overlap_names = adj_list.intersection(table_2_bi)
+
+
+def make_venn_diagram(set1,set2,label1,label2,n):
+    overlap = set1.intersection(set2)
+    total = set1.union(set2)
+    smaller_size = min(len(set1),len(set2))
+    # setup the figure
+    fig, ax = plt.subplots()
+    ax.set_title("Percent shared (overall and of smaller set): "+str(len(overlap)/len(total))+", "+str(len(overlap)/smaller_size)
+                 , fontsize=10)
+    # make the venn diagram
+    v = venn2([set1, set2],
+              set_labels=[label1, label2])
+    # adjust labels from counts to names
+    v.get_label_by_id('10').set_text("\n".join(list(set1)[:n]))
+    v.get_label_by_id('01').set_text("\n".join(list(set2)[:n]))
+    v.get_label_by_id('11').set_text("\n".join(list(overlap)[:n]))
+    
+    df = pd.DataFrame(data=[set1,overlap,set2])
+    df = df.T
+    df.columns=[label1,"Overlap",label2]
+    
+    #print(df.to_latex())
+    print(df.to_markdown())
+    return df
+
+#call like, e.g. 
+#make_venn_diagram(adj_list,table_2_bi,"Character Adjectives","Goldberg FFM Adj",5)
+    
+
+
